cee3d3e1810310bc9c5a5775f78876797915c39b
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-04 23:55:56 +0100

    Create NewInterview.java

---
+++ app/src/main/java/com/Denzo/firl/interviewActivity/crtNewInterview/NewInterview.java
@@ -0,0 +1,16 @@
+package com.Denzo.firl.interviewActivity.crtNewInterview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.Denzo.firl.R;
+
+public class NewInterview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_interview);
+    }
+}
