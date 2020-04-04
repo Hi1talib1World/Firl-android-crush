7b61010c9eb757c3e13aaba9141982027e8a4891
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-04 23:56:09 +0100

    Create interview.java

---
+++ app/src/main/java/com/Denzo/firl/interviewActivity/interview.java
@@ -0,0 +1,16 @@
+package com.Denzo.firl.interviewActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.Denzo.firl.R;
+
+public class interview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interview);
+    }
+}
