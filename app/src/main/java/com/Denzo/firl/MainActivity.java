b707491cc2a6f91f5df685d4070ec19427c2e9d3
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-04 07:35:26 +0100

    Update MainActivity.java

--- app/src/main/java/com/Denzo/firl/MainActivity.java
+++ app/src/main/java/com/Denzo/firl/MainActivity.java
@@ -2,7 +2,11 @@ package com.Denzo.firl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Click action
+               
+            }
+        });
     }
 }
