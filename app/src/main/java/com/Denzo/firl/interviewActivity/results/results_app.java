71a1c6b3663f1b43f4857324d55a8411869f25a9
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-04 23:56:14 +0100

    Create results_app.java

---
+++ app/src/main/java/com/Denzo/firl/interviewActivity/results/results_app.java
@@ -0,0 +1,16 @@
+package com.Denzo.firl.interviewActivity.results;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.Denzo.firl.R;
+
+public class results_app extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results_app);
+    }
+}
