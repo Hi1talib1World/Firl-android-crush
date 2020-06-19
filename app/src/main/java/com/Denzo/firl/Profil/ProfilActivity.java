5218ca929480932f29c020879f13d6f1dcab0aeb
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-19 22:33:27 +0100

    Update ProfilActivity.java

--- app/src/main/java/com/Denzo/firl/Profil/ProfilActivity.java
+++ app/src/main/java/com/Denzo/firl/Profil/ProfilActivity.java
@@ -4,15 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.Denzo.firl.R;
 
+import butterknife.BindView;
+import jp.wasabeef.blurry.Blurry;
+
 public class ProfilActivity extends AppCompatActivity {
+    @BindView(R.id.ll) LinearLayout LinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
         ImageView Iv = (ImageView) findViewById(R.id.img1);
+
+        Blurry.with(getApplicationContext())
+                .radius(25)
+                .async()
+                .animate(500)
+                .sampling(8)
+                .onto(LinearLayout);
     }
 }
