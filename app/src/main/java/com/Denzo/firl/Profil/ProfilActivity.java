5d07e4db9087cd05e413bbfc86f5a9d9a615ca69
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-19 19:52:44 +0100

    Create ProfilActivity.java

---
+++ app/src/main/java/com/Denzo/firl/Profil/ProfilActivity.java
@@ -0,0 +1,14 @@
+package com.Denzo.firl.Profil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+    }
+}
