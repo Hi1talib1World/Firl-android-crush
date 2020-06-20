353a418c039e34411f2443e760abd68cdf5a20e8
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-20 21:40:11 +0100

    Update TabbsActivity.java

--- app/src/main/java/com/Denzo/firl/feed/TabbsActivity.java
+++ app/src/main/java/com/Denzo/firl/feed/TabbsActivity.java
@@ -2,20 +2,35 @@ package com.Denzo.firl.feed;
 
 import android.os.Bundle;
 
+import com.Denzo.firl.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.Denzo.firl.feed.ui.main.SectionsPagerAdapter;
 
 public class TabbsActivity extends AppCompatActivity {
+
+
+    private PostsAdapter postsAdapter;
+    private RecyclerView recyclerView;
+    private FloatingActionButton floatingActionButton;
+
+    private TextView newPostsCounterTextView;
+    private boolean counterAnimationInProgress = false;
+    private ProgressBar progressBar;
+    private SwipeRefreshLayout swipeContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +41,6 @@ public class TabbsActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
