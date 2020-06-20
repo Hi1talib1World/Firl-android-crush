142487d37b33d49174ffc17457682e11d81af055
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-20 21:57:38 +0100

    Create CreatePostPresenter.java

---
+++ app/src/main/java/com/Denzo/firl/feed/CreatePostPresenter.java
@@ -0,0 +1,35 @@
+package com.Denzo.firl.feed;
+
+import android.content.Context;
+
+import com.Denzo.firl.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CreatePostPresenter extends BaseCreatePostPresenter<CreatePostView> {
+
+    public CreatePostPresenter(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected int getSaveFailMessage() {
+        return R.string.error_fail_create_post;
+    }
+
+    @Override
+    protected void savePost(String title, String description) {
+        ifViewAttached(view -> {
+            view.showProgress(R.string.message_creating_post);
+            Post post = new Post();
+            post.setTitle(title);
+            post.setDescription(description);
+            post.setAuthorId(FirebaseAuth.getInstance().getCurrentUser().getUid());
+            postManager.createOrUpdatePostWithImage(view.getImageUri(), this, post);
+        });
+    }
+
+    @Override
+    protected boolean isImageRequired() {
+        return true;
+    }
+}
