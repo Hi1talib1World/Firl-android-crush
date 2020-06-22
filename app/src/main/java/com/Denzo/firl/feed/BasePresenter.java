7d7b2a8e1e02bb5f1ee82a0f73beddc02126c8dc
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-22 18:21:31 +0100

    Create BasePresenter.java

---
+++ app/src/main/java/com/Denzo/firl/feed/BasePresenter.java
@@ -0,0 +1,70 @@
+package com.Denzo.firl.feed;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import com.Denzo.firl.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class BasePresenter <T extends BaseView & MvpView> extends MvpBasePresenter<T> {
+
+    protected String TAG = this.getClass().getSimpleName();
+
+    protected Context context;
+    private ProfileManager profileManager;
+
+    public BasePresenter(Context context) {
+        this.context = context;
+        profileManager = ProfileManager.getInstance(context);
+    }
+
+    public boolean checkInternetConnection() {
+        return checkInternetConnection(null);
+    }
+
+    public boolean checkInternetConnection(@Nullable View anchorView) {
+        boolean hasInternetConnection = hasInternetConnection();
+        if (!hasInternetConnection) {
+            ifViewAttached(view -> {
+                if (anchorView != null) {
+                    view.showSnackBar(anchorView, R.string.internet_connection_failed);
+                } else {
+                    view.showSnackBar(R.string.internet_connection_failed);
+                }
+            });
+        }
+
+        return hasInternetConnection;
+    }
+
+    public boolean hasInternetConnection() {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
+
+    public boolean checkAuthorization(){
+        ProfileStatus profileStatus = profileManager.checkProfile();
+        if (profileStatus.equals(ProfileStatus.NOT_AUTHORIZED) || profileStatus.equals(ProfileStatus.NO_PROFILE)) {
+            ifViewAttached(BaseView::startLoginActivity);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void doAuthorization(ProfileStatus status) {
+        if (status.equals(ProfileStatus.NOT_AUTHORIZED) || status.equals(ProfileStatus.NO_PROFILE)) {
+            ifViewAttached(BaseView::startLoginActivity);
+        }
+    }
+
+    protected String getCurrentUserId() {
+        return FirebaseAuth.getInstance().getUid();
+    }
+
+}
