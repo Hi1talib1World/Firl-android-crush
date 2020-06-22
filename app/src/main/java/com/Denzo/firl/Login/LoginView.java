145722188845a2881b47ac0f727f8e6010271bf9
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-22 18:21:44 +0100

    Update LoginView.java

--- app/src/main/java/com/Denzo/firl/Login/LoginView.java
+++ app/src/main/java/com/Denzo/firl/Login/LoginView.java
@@ -1,4 +1,17 @@
 package com.Denzo.firl.Login;
 
-public class LoginView {
+
+import com.Denzo.firl.feed.BaseView;
+import com.google.firebase.auth.AuthCredential;
+
+public interface LoginView extends BaseView {
+    void startCreateProfileActivity();
+
+    void signInWithGoogle();
+
+    void signInWithFacebook();
+
+    void setProfilePhotoUrl(String url);
+
+    void firebaseAuthWithCredentials(AuthCredential credential);
 }
