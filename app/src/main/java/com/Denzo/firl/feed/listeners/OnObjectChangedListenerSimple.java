c5266ebfac5b452f391ab65f1ed13ae4e749ed12
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-23 03:47:06 +0100

    Update OnObjectChangedListenerSimple.java

--- app/src/main/java/com/Denzo/firl/feed/listeners/OnObjectChangedListenerSimple.java
+++ app/src/main/java/com/Denzo/firl/feed/listeners/OnObjectChangedListenerSimple.java
@@ -1,4 +1,8 @@
 package com.Denzo.firl.feed.listeners;
 
-public class OnObjectChangedListenerSimple {
+public abstract class OnObjectChangedListenerSimple<T> implements OnObjectChangedListener<T> {
+
+    @Override
+    public void onError(String errorText) {
+    }
 }
