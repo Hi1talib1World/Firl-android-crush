41c1d75483fa32515954b31128faba6da28a4536
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-23 03:31:12 +0100

    Update ProfileInteractor.java

--- app/src/main/java/com/Denzo/firl/feed/ProfileInteractor.java
+++ app/src/main/java/com/Denzo/firl/feed/ProfileInteractor.java
@@ -1,9 +1,12 @@
 package com.Denzo.firl.feed;
 
+import android.content.Context;
 import android.net.Uri;
 
 import com.Denzo.firl.Profile;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
