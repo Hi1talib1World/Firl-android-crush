b1da9a08444ea8def1b59ae74b63ccd9ff6579d6
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-23 04:29:54 +0100

    Create LazyLoading.java

---
+++ app/src/main/java/com/Denzo/firl/feed/model/LazyLoading.java
@@ -0,0 +1,8 @@
+package com.Denzo.firl.feed.model;
+
+import com.Denzo.firl.feed.enums.ItemType;
+
+public class LazyLoading {
+    ItemType getItemType();
+    void setItemType(ItemType itemType);
+}
