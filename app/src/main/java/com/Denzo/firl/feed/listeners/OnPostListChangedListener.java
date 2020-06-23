243554b629e610827a89de300bdd2fb829319b1e
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-23 04:20:48 +0100

    Update OnPostListChangedListener.java

--- app/src/main/java/com/Denzo/firl/feed/listeners/OnPostListChangedListener.java
+++ app/src/main/java/com/Denzo/firl/feed/listeners/OnPostListChangedListener.java
@@ -1,5 +1,7 @@
 package com.Denzo.firl.feed.listeners;
 
+import com.Denzo.firl.feed.model.PostListResult;
+
 public interface OnPostListChangedListener<Post> {
 
     public void onListChanged(PostListResult result);
