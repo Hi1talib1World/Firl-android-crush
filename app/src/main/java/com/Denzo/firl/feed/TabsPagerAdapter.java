80286b035d1f9fa9563735559d34defb7a8ee286
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-24 11:01:10 +0100

    Create TabsPagerAdapter.java

---
+++ app/src/main/java/com/Denzo/firl/feed/TabsPagerAdapter.java
@@ -0,0 +1,59 @@
+package com.Denzo.firl.feed;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+import java.util.ArrayList;
+
+public class TabsPagerAdapter extends SmartFragmentStatePagerAdapter {
+
+    private final ArrayList<TabInfo> mTabs = new ArrayList<>();
+    private final FragmentActivity mActivity;
+
+    public TabsPagerAdapter(FragmentActivity activity, FragmentManager fragmentManager) {
+        super(fragmentManager);
+        mActivity = activity;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mTabs.get(position).getFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return mTabs.size();
+    }
+
+    public void addTab(Class<? extends Searchable> clss, @Nullable Bundle args, String title) {
+        mTabs.add(new TabInfo(title, Fragment.instantiate(mActivity, clss.getName(), args)));
+    }
+
+    // Returns the page title for the top indicator
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTabs.get(position).getTitle();
+    }
+
+    static final class TabInfo {
+        String title;
+        Fragment fragment;
+
+        public TabInfo(String title, Fragment fragment) {
+            this.title = title;
+            this.fragment = fragment;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public Fragment getFragment() {
+            return fragment;
+        }
+    }
+}
