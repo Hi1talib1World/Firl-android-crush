f907dea0eaffb51f5931c537747bb304fce2e267
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-24 20:15:18 +0100

    Create BasePostsAdapter.java

---
+++ app/src/main/java/com/Denzo/firl/feed/BasePostsAdapter.java
@@ -0,0 +1,61 @@
+package com.Denzo.firl.feed;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.Denzo.firl.feed.listeners.OnPostChangedListener;
+import com.Denzo.firl.feed.model.Post;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public abstract class BasePostsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    public static final String TAG = BasePostsAdapter.class.getSimpleName();
+
+    protected List<Post> postList = new LinkedList<>();
+    protected BaseActivity activity;
+    protected int selectedPostPosition = RecyclerView.NO_POSITION;
+
+    public BasePostsAdapter(BaseActivity activity) {
+        this.activity = activity;
+    }
+
+    protected void cleanSelectedPostInformation() {
+        selectedPostPosition = -1;
+    }
+
+    @Override
+    public int getItemCount() {
+        return postList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return postList.get(position).getItemType().getTypeCode();
+    }
+
+    protected Post getItemByPosition(int position) {
+        return postList.get(position);
+    }
+
+    private OnPostChangedListener createOnPostChangeListener(final int postPosition) {
+        return new OnPostChangedListener() {
+            @Override
+            public void onObjectChanged(Post obj) {
+                postList.set(postPosition, obj);
+                notifyItemChanged(postPosition);
+            }
+
+            @Override
+            public void onError(String errorText) {
+                LogUtil.logDebug(TAG, errorText);
+            }
+        };
+    }
+
+    public void updateSelectedPost() {
+        if (selectedPostPosition != RecyclerView.NO_POSITION) {
+            Post selectedPost = getItemByPosition(selectedPostPosition);
+            PostManager.getInstance(activity).getSinglePostValue(selectedPost.getId(), createOnPostChangeListener(selectedPostPosition));
+        }
+    }
+}
