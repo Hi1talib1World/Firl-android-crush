96c76f041be2c3b942929dc4327b770f2e54386a
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-27 18:14:46 +0100

    Create ApplicationHelper.java

---
+++ app/src/main/java/com/Denzo/firl/ApplicationHelper.java
@@ -0,0 +1,18 @@
+package com.Denzo.firl;
+
+import com.Denzo.firl.feed.DatabaseHelper;
+
+public class ApplicationHelper {
+
+    private static final String TAG = ApplicationHelper.class.getSimpleName();
+    private static DatabaseHelper databaseHelper;
+
+    public static DatabaseHelper getDatabaseHelper() {
+        return databaseHelper;
+    }
+
+    public static void initDatabaseHelper(android.app.Application application) {
+        databaseHelper = DatabaseHelper.getInstance(application);
+        databaseHelper.init();
+    }
+}
