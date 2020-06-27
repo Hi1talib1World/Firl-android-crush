69d27c6508ac7ae631a4706555d2edc70b94f0b4
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-27 17:39:22 +0100

    Create FollowingPost.java

---
+++ app/src/main/java/com/Denzo/firl/feed/model/FollowingPost.java
@@ -0,0 +1,19 @@
+package com.Denzo.firl.feed.model;
+
+
+public class FollowingPost {
+
+    private String postId;
+
+    public FollowingPost() {
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+}
