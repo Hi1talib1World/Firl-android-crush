5baf5cf117e32af86ee37873a76747be446944ba
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-27 18:14:52 +0100

    Update LazyLoading.java

--- app/src/main/java/com/Denzo/firl/feed/model/LazyLoading.java
+++ app/src/main/java/com/Denzo/firl/feed/model/LazyLoading.java
@@ -2,7 +2,7 @@ package com.Denzo.firl.feed.model;
 
 import com.Denzo.firl.feed.enums.ItemType;
 
-public class LazyLoading {
+public interface  LazyLoading {
     ItemType getItemType();
     void setItemType(ItemType itemType);
 }
