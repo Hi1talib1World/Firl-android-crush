7bc8ee3389cee3df5360cda4a29fd19cd55db262
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-27 18:58:10 +0100

    Create Following.java

---
+++ app/src/main/java/com/Denzo/firl/feed/model/Following.java
@@ -0,0 +1,33 @@
+package com.Denzo.firl.feed.model;
+
+
+import java.util.Calendar;
+
+public class Following {
+    private String profileId;
+    private long createdDate;
+
+    public Following() {
+    }
+
+    public Following(String profileId) {
+        this.profileId = profileId;
+        this.createdDate = Calendar.getInstance().getTimeInMillis();
+    }
+
+    public String getProfileId() {
+        return profileId;
+    }
+
+    public void setProfileId(String profileId) {
+        this.profileId = profileId;
+    }
+
+    public long getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(long createdDate) {
+        this.createdDate = createdDate;
+    }
+}
