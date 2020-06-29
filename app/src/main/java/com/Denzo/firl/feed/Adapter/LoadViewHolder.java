875f8e385e69f665f2f828b0b29ec17b4dfd41eb
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-29 23:07:42 +0100

    Create LoadViewHolder.java

---
+++ app/src/main/java/com/Denzo/firl/feed/Adapter/LoadViewHolder.java
@@ -0,0 +1,12 @@
+package com.Denzo.firl.feed.Adapter;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public class LoadViewHolder extends RecyclerView.ViewHolder {
+    public LoadViewHolder(View itemView) {
+        super(itemView);
+    }
+}
