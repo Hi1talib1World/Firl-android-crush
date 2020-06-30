e68e7fa6b9489367493a5e44c9e7748f6514c7d0
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-30 15:15:53 +0100

    Create TabbsActivity.java

---
+++ app/src/main/java/com/Denzo/firl/feed/main/TabbsActivity.java
@@ -0,0 +1,302 @@
+package com.Denzo.firl.feed.main;
+
+import android.annotation.SuppressLint;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.Denzo.firl.Profil.ProfilActivity;
+import com.Denzo.firl.R;
+
+import com.Denzo.firl.feed.Adapter.PostsAdapter;
+import com.Denzo.firl.feed.Base.BaseActivity;
+import com.Denzo.firl.feed.CreatePostActivity;
+
+import com.Denzo.firl.feed.followPosts.FollowingPostsActivity;
+import com.Denzo.firl.feed.model.Post;
+import com.Denzo.firl.feed.utils.AnimationUtils;
+import com.Denzo.firl.postDetails.PostDetailsActivity;
+import com.Denzo.firl.search.SearchActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SimpleItemAnimator;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import androidx.viewpager.widget.ViewPager;
+
+
+
+import com.Denzo.firl.feed.ui.main.SectionsPagerAdapter;
+
+public class TabbsActivity extends BaseActivity<MainView, MainPresenter> implements MainView {
+
+
+    private PostsAdapter postsAdapter;
+    private RecyclerView recyclerView;
+    private FloatingActionButton floatingActionButton;
+
+    private TextView newPostsCounterTextView;
+    private boolean counterAnimationInProgress = false;
+    private ProgressBar progressBar;
+    private SwipeRefreshLayout swipeContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabbs);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        presenter.updateNewPostCounter();
+    }
+
+    @NonNull
+    @Override
+    public MainPresenter createPresenter() {
+        if (presenter == null) {
+            return new MainPresenter(this);
+        }
+        return presenter;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case ProfilActivity.CREATE_POST_FROM_PROFILE_REQUEST:
+                    refreshPostList();
+                    break;
+                case CreatePostActivity.CREATE_NEW_POST_REQUEST:
+                    presenter.onPostCreated();
+                    break;
+
+                case PostDetailsActivity.UPDATE_POST_REQUEST:
+                    presenter.onPostUpdated(data);
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        attemptToExitIfRoot(floatingActionButton);
+    }
+
+    public void refreshPostList() {
+        postsAdapter.loadFirstPage();
+        if (postsAdapter.getItemCount() > 0) {
+            recyclerView.scrollToPosition(0);
+        }
+    }
+
+    @Override
+    public void removePost() {
+        postsAdapter.removeSelectedPost();
+    }
+
+    @Override
+    public void updatePost() {
+        postsAdapter.updateSelectedPost();
+    }
+
+    @Override
+    public void showCounterView(int count) {
+        AnimationUtils.showViewByScaleAndVisibility(newPostsCounterTextView);
+        String counterFormat = getResources().getQuantityString(R.plurals.new_posts_counter_format, count, count);
+        newPostsCounterTextView.setText(String.format(counterFormat, count));
+    }
+
+    private void initContentView() {
+        if (recyclerView == null) {
+            progressBar = findViewById(R.id.progressBar);
+            swipeContainer = findViewById(R.id.swipeContainer);
+
+            initFloatingActionButton();
+            initPostListRecyclerView();
+            initPostCounter();
+        }
+    }
+
+    private void initFloatingActionButton() {
+        floatingActionButton = findViewById(R.id.addNewPostFab);
+        if (floatingActionButton != null) {
+            floatingActionButton.setOnClickListener(v -> presenter.onCreatePostClickAction(floatingActionButton));
+        }
+    }
+
+    private void initPostListRecyclerView() {
+        recyclerView = findViewById(R.id.recycler_view);
+        postsAdapter = new PostsAdapter(this, swipeContainer);
+        postsAdapter.setCallback(new PostsAdapter.Callback() {
+            @Override
+            public void onItemClick(final Post post, final View view) {
+                presenter.onPostClicked(post, view);
+            }
+
+            @Override
+            public void onListLoadingFinished() {
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAuthorClick(String authorId, View view) {
+                openProfileActivity(authorId, view);
+            }
+
+            @Override
+            public void onCanceled(String message) {
+                progressBar.setVisibility(View.GONE);
+                showToast(message);
+            }
+        });
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        ((SimpleItemAnimator) recyclerView.getItemAnimator()).setSupportsChangeAnimations(false);
+        recyclerView.setAdapter(postsAdapter);
+        postsAdapter.loadFirstPage();
+    }
+
+    private void initPostCounter() {
+        newPostsCounterTextView = findViewById(R.id.newPostsCounterTextView);
+        newPostsCounterTextView.setOnClickListener(v -> refreshPostList());
+
+        presenter.initPostCounter();
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                hideCounterView();
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
+    }
+
+    @Override
+    public void hideCounterView() {
+        if (!counterAnimationInProgress && newPostsCounterTextView.getVisibility() == View.VISIBLE) {
+            counterAnimationInProgress = true;
+            AlphaAnimation alphaAnimation = AnimationUtils.hideViewByAlpha(newPostsCounterTextView);
+            alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    counterAnimationInProgress = false;
+                    newPostsCounterTextView.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+            alphaAnimation.start();
+        }
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public void openPostDetailsActivity(Post post, View v) {
+        Intent intent = new Intent(TabbsActivity.this, PostDetailsActivity.class);
+        intent.putExtra(PostDetailsActivity.POST_ID_EXTRA_KEY, post.getId());
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            View imageView = v.findViewById(R.id.postImageView);
+            View authorImageView = v.findViewById(R.id.authorImageView);
+
+            ActivityOptions options = ActivityOptions.
+                    makeSceneTransitionAnimation(TabbsActivity.this,
+                            new android.util.Pair<>(imageView, getString(R.string.post_image_transition_name)),
+                            new android.util.Pair<>(authorImageView, getString(R.string.post_author_image_transition_name))
+                    );
+            startActivityForResult(intent, PostDetailsActivity.UPDATE_POST_REQUEST, options.toBundle());
+        } else {
+            startActivityForResult(intent, PostDetailsActivity.UPDATE_POST_REQUEST);
+        }
+    }
+
+    public void showFloatButtonRelatedSnackBar(int messageId) {
+        showSnackBar(floatingActionButton, messageId);
+    }
+
+    @Override
+    public void openCreatePostActivity() {
+        Intent intent = new Intent(this, CreatePostActivity.class);
+        startActivityForResult(intent, CreatePostActivity.CREATE_NEW_POST_REQUEST);
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public void openProfileActivity(String userId, View view) {
+        Intent intent = new Intent(TabbsActivity.this, ProfilActivity.class);
+        intent.putExtra(ProfilActivity.USER_ID_EXTRA_KEY, userId);
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && view != null) {
+
+            View authorImageView = view.findViewById(R.id.authorImageView);
+
+            ActivityOptions options = ActivityOptions.
+                    makeSceneTransitionAnimation(TabbsActivity.this,
+                            new android.util.Pair<>(authorImageView, getString(R.string.post_author_image_transition_name)));
+            startActivityForResult(intent, ProfilActivity.CREATE_POST_FROM_PROFILE_REQUEST, options.toBundle());
+        } else {
+            startActivityForResult(intent, ProfilActivity.CREATE_POST_FROM_PROFILE_REQUEST);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.profile:
+                presenter.onProfileMenuActionClicked();
+                return true;
+
+            case R.id.followingPosts:
+                Intent followingPosts = new Intent(this, FollowingPostsActivity.class);
+                startActivity(followingPosts);
+                return true;
+
+            case R.id.search:
+                Intent searchIntent = new Intent(this, SearchActivity.class);
+                startActivity(searchIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
