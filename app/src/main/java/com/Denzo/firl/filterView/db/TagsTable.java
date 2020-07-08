9f1e20f279da43c4d3b4589459b22782a1d86b40
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-07-08 05:05:39 +0100

    Create TagsTable.java

---
+++ app/src/main/java/com/Denzo/firl/filterView/db/TagsTable.java
@@ -0,0 +1,8 @@
+package com.Denzo.firl.filterView.db;
+
+import android.provider.BaseColumns;
+
+public class TagsTable implements BaseColumns {
+    public static final String TABLE_NAME = "tags";
+    public static final String TAG = "tag";
+}
