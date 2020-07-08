5779afafffcb00313a1f9618926b14c259fbbc76
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-07-08 05:27:20 +0100

    Update DatabaseHelper.java

--- app/src/main/java/com/Denzo/firl/filterView/db/DatabaseHelper.java
+++ app/src/main/java/com/Denzo/firl/filterView/db/DatabaseHelper.java
@@ -1,4 +1,25 @@
 package com.Denzo.firl.filterView.db;
 
-public class DatabaseHelper {
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public DatabaseHelper(Context context) {
+        super(context, "demo.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TagsTable.TABLE_NAME
+                + "("
+                + TagsTable._ID + " INTEGER PRIMARY KEY,"
+                + TagsTable.TAG + " TEXT"
+                + ");");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
 }
