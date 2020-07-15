971d1e20322b436b09905f1d7449cd8c08647edf
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-07-15 22:35:22 +0100

    Update imgFilter.java

--- app/src/main/java/com/Denzo/firl/ImageFilter/imgFilter.java
+++ app/src/main/java/com/Denzo/firl/ImageFilter/imgFilter.java
@@ -21,7 +21,7 @@ public class imgFilter extends AppCompatActivity implements View.OnClickListener
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_imgFilter);
+        setContentView(R.layout.activity_imgfilter);
         imageView=(ImageView)findViewById(R.id.imageView);
         iBitmap= BitmapFactory.decodeResource(getResources(),R.drawable.photo);
         photoFilter=new PhotoFilter();
