bc5d9996bb79c511b1f9921f298a3e9965b4d431
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-12-20 23:11:09 +0100

    Update IIssueTimelineContract.java

--- app/src/main/java/com/Denzo/firl/contract/IIssueTimelineContract.java
+++ app/src/main/java/com/Denzo/firl/contract/IIssueTimelineContract.java
@@ -1,4 +1,17 @@
 package com.Denzo.firl.contract;
 
-public class IIssueTimelineContract {
+public interface IIssueTimelineContract {
+
+    interface View extends IBaseContract.View, IBaseListContract.View {
+        void showTimeline(ArrayList<IssueEvent> events);
+        void showEditCommentPage(String commentId, String body);
+    }
+
+    interface Presenter extends IBaseContract.Presenter<IIssueTimelineContract.View> {
+        void loadTimeline(int page, boolean isReload);
+        boolean isEditAndDeleteEnable(int position);
+        void deleteComment(String commentId);
+        void editComment(String commentId, String body);
+    }
+
 }
