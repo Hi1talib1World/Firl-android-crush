0c6b5b0cbe6e2647bb5f0ed47d8766da57b5a352
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-12-20 23:11:02 +0100

    Update IIssueDetailContract.java

--- app/src/main/java/com/Denzo/firl/contract/IIssueDetailContract.java
+++ app/src/main/java/com/Denzo/firl/contract/IIssueDetailContract.java
@@ -1,4 +1,16 @@
 package com.Denzo.firl.contract;
 
-public class IIssueDetailContract {
+public interface IIssueDetailContract {
+
+    interface View extends IBaseContract.View{
+        void showIssue(@NonNull Issue issue);
+        void showAddedComment(@NonNull IssueEvent event);
+        void showAddCommentPage(@Nullable String text);
+    }
+
+    interface Presenter extends IBaseContract.Presenter<IIssueDetailContract.View> {
+        void addComment(@NonNull String text);
+        void toggleIssueState();
+    }
+
 }
