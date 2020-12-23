35b3027dbcf4dd4ae5db8596d7bdca5aa3ae94b9
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-12-23 23:03:44 +0100

    Update MyAppGlideModel.java

--- app/src/main/java/com/Denzo/firl/common/MyAppGlideModel.java
+++ app/src/main/java/com/Denzo/firl/common/MyAppGlideModel.java
@@ -1,4 +1,24 @@
 package com.Denzo.firl.common;
 
-public class MyAppGlideModel {
+@GlideModule
+public class MyAppGlideModel extends AppGlideModule {
+
+    @Override
+    public boolean isManifestParsingEnabled() {
+        return super.isManifestParsingEnabled();
+    }
+
+    @Override
+    public void applyOptions(Context context, GlideBuilder builder) {
+        super.applyOptions(context, builder);
+        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, AppConfig.IMAGE_MAX_CACHE_SIZE));
+        RequestOptions requestOptions = RequestOptions.placeholderOf(R.mipmap.logo);
+        builder.setDefaultRequestOptions(requestOptions);
+    }
+
+    @Override
+    public void registerComponents(Context context, Glide glide, Registry registry) {
+        super.registerComponents(context, glide, registry);
+    }
+
 }
