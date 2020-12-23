d41c31b826b919a9ce2baee3874d59019af883f2
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-12-23 23:48:32 +0100

    Update AppEventBus.java

--- app/src/main/java/com/Denzo/firl/common/AppEventBus.java
+++ app/src/main/java/com/Denzo/firl/common/AppEventBus.java
@@ -1,4 +1,20 @@
 package com.Denzo.firl.common;
 
-public class AppEventBus {
+public enum  AppEventBus {
+    INSTANCE;
+
+    AppEventBus(){
+        init();
+    }
+
+    private EventBus eventBus ;
+
+    private void init(){
+        eventBus = EventBus.builder()
+                .installDefaultEventBus();
+    }
+
+    public EventBus getEventBus() {
+        return eventBus;
+    }
 }
