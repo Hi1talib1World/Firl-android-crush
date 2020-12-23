8fb0810d0d074177827cfd782e6f0c4a2a31d647
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-12-23 23:04:56 +0100

    Update SizedTreeSet.java

--- app/src/main/java/com/Denzo/firl/common/SizedTreeSet.java
+++ app/src/main/java/com/Denzo/firl/common/SizedTreeSet.java
@@ -1,4 +1,25 @@
 package com.Denzo.firl.common;
 
-public class SizedTreeSet {
+public class SizedTreeSet<E> extends TreeSet<E> {
+
+    private int maxSize ;
+
+    public SizedTreeSet(int maxSize){
+        super();
+        this.maxSize = maxSize;
+    }
+
+    @Override
+    public boolean add(E e) {
+        if(size() >= maxSize && !contains(e)){
+            E last = null;
+            Iterator<E> iterator = iterator();
+            for( ; iterator.hasNext(); ){
+                last = iterator.next();
+            }
+            remove(last);
+        }
+        return super.add(e);
+    }
+
 }
