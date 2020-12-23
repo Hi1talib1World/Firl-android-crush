00393184037a7e904de42416918ece8eda6686b2
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-12-23 23:45:14 +0100

    Update Event.java

--- app/src/main/java/com/Denzo/firl/common/Event.java
+++ app/src/main/java/com/Denzo/firl/common/Event.java
@@ -1,4 +1,47 @@
 package com.Denzo.firl.common;
 
 public class Event {
+
+    /**
+     * 网络状态改变事件
+     */
+    public static class NetChangedEvent{
+        public int preNetStatus;
+        public int curNetStatus;
+        public NetChangedEvent(int preNetStatus, int curNetStatus){
+            this.preNetStatus = preNetStatus;
+            this.curNetStatus = curNetStatus;
+        }
+    }
+
+    public static class ServerStatusChangedEvent{
+        public final static int SERVER_START = 0;
+
+        public final static int SERVER_STOP = 1;
+
+        public final static int SERVER_STARTED = 2;
+
+        public int serverStatus;
+
+        public ServerStatusChangedEvent(int serverStatus) {
+            this.serverStatus = serverStatus;
+        }
+    }
+
+    public static class RepoInfoUpdatedEvent{
+        public Repository repository;
+
+        public RepoInfoUpdatedEvent(Repository repository) {
+            this.repository = repository;
+        }
+    }
+
+    public static class SearchEvent{
+        public SearchModel searchModel;
+
+        public SearchEvent(SearchModel searchModel) {
+            this.searchModel = searchModel;
+        }
+    }
+
 }
