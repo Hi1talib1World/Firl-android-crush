f36ae20692b1a9ddc6b83ea1260eafabcd6d0225
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-01-22 23:13:34 +0100

    Create WindowUtil.java

---
+++ app/src/main/java/com/Denzo/firl/feed/utils/WindowUtil.java
@@ -0,0 +1,52 @@
+package com.Denzo.firl.feed.utils;
+
+public class WindowUtil {
+
+    public static int screenWidth = 0;
+
+
+    public static int screenHeight = 0;
+
+
+    public static int dipToPx(@NonNull Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+
+    public static int pxToDip(@NonNull Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    public static int getStatusHeight(@NonNull Activity activity){
+        int statusHeight = 0;
+        Rect localRect = new Rect();
+        activity.getWindow().getDecorView().getWindowVisibleDisplayFrame(localRect);
+        statusHeight = localRect.top;
+        if (0 == statusHeight){
+            Class<?> localClass;
+            try {
+                localClass = Class.forName("com.android.internal.R$dimen");
+                Object localObject = localClass.newInstance();
+                int i5 = Integer.parseInt(localClass.getField("status_bar_height").get(localObject).toString());
+                statusHeight = activity.getResources().getDimensionPixelSize(i5);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            }
+        }
+        return statusHeight;
+    }
+}
