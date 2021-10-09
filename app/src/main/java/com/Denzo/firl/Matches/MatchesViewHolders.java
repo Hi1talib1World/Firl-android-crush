9514b4a094ab3bd21b4397a23d8dcb6bf2c25164
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-10-09 06:55:13 +0100

    Create MatchesViewHolders.java

---
+++ app/src/main/java/com/Denzo/firl/Matches/MatchesViewHolders.java
@@ -0,0 +1,36 @@
+package com.Denzo.firl.Matches;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.Denzo.firl.R;
+import com.Denzo.firl.chat.ChatActivity;
+
+public class MatchesViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public TextView mMatchId, mMatchName;
+    public ImageView mMatchImage;
+    public MatchesViewHolders(View itemView) {
+        super(itemView);
+        itemView.setOnClickListener(this);
+
+        mMatchId = (TextView) itemView.findViewById(R.id.Matchid);
+        mMatchName = (TextView) itemView.findViewById(R.id.MatchName);
+
+        mMatchImage = (ImageView) itemView.findViewById(R.id.MatchImage);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(view.getContext(), ChatActivity.class);
+        Bundle b = new Bundle();
+        b.putString("matchId", mMatchId.getText().toString());
+        intent.putExtras(b);
+        view.getContext().startActivity(intent);
+    }
+}
