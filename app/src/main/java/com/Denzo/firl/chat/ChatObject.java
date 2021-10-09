b91a45a9c6b6dda35c848e8e223906a53884ad3a
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-10-09 06:57:43 +0100

    Create ChatObject.java

---
+++ app/src/main/java/com/Denzo/firl/chat/ChatObject.java
@@ -0,0 +1,25 @@
+package com.Denzo.firl.chat;
+
+
+public class ChatObject {
+    private String message;
+    private Boolean currentUser;
+    public ChatObject(String message, Boolean currentUser){
+        this.message = message;
+        this.currentUser = currentUser;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+    public void setMessage(String userID){
+        this.message = message;
+    }
+
+    public Boolean getCurrentUser(){
+        return currentUser;
+    }
+    public void setCurrentUser(Boolean currentUser){
+        this.currentUser = currentUser;
+    }
+}
