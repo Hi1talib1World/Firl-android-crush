980b07d43ce05f234d73649e7b964f6d0ead8ac1
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-10-13 17:44:27 +0100

    Update MainActivity.java

--- app/src/main/java/com/Denzo/firl/MainActivity.java
+++ app/src/main/java/com/Denzo/firl/MainActivity.java
@@ -13,26 +13,18 @@ import java.util.ArrayList;
 import xute.storyview.StoryModel;
 import xute.storyview.StoryView;
 
-
 public class MainActivity extends AppCompatActivity {
-
-    StoryView storyView;   // get the object for StoryView
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-        storyView = findViewById(R.id.storyView); // find the XML view using findViewById
-        storyView.resetStoryVisits(); // reset the storyview
-
-        ArrayList<StoryModel> StoriesList = new ArrayList<>();  // create a Array list of Stories
-        StoriesList.add(new StoryModel("https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80","Status 1","Yesterday"));
-        StoriesList.add(new StoryModel("https://www.bigstockphoto.com/images/homepage/module-6.jpg","Status 2","10:15 PM"));
-        StoriesList.add(new StoryModel("https://www.gettyimages.com/gi-resources/images/500px/983794168.jpg","Satus 3","Today,2:31 PM"));
-        storyView.setImageUris(StoriesList);  // finally set the stories to storyview
+        StoryView storyView = findViewById(R.id.storyView);
+        storyView.resetStoryVisits();
+        ArrayList<StoryModel> uris = new ArrayList<>();
+        uris.add(new StoryModel("https://picsum.photos/200/300", "Ankit Kumar", "12:00 PM"));
+        uris.add(new StoryModel("https://picsum.photos/200/300", "Ankit Kumar", "12:00 PM"));
+        uris.add(new StoryModel("https://picsum.photos/200/300", "Ankit Kumar", "12:00 PM"));
+        uris.add(new StoryModel("https://picsum.photos/200/300", "Ankit Kumar", "12:00 PM"));
     }
 }
