a1f8b3f7151284500a30586c50b68ea83b5923d1
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-03 18:04:58 +0100

    Update RegistrationActivity.java

--- app/src/main/java/com/Denzo/firl/RegistrationActivity.java
+++ app/src/main/java/com/Denzo/firl/RegistrationActivity.java
@@ -3,15 +3,26 @@ package com.Denzo.firl;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistrationActivity extends AppCompatActivity {
 
