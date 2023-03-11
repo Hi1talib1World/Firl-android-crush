c6ac00dc6424c455bed342c1112706053551e292
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2023-03-11 21:35:56 +0100

    Update LoginActivity.java

--- app/src/main/java/com/Denzo/firl/Login/LoginActivity.java
+++ app/src/main/java/com/Denzo/firl/Login/LoginActivity.java
@@ -12,15 +12,14 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
 import com.Denzo.firl.Login.register.SignupActivity;
 import com.Denzo.firl.MainActivity;
 import com.Denzo.firl.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
 
 public class LoginActivity extends AppCompatActivity {
 
