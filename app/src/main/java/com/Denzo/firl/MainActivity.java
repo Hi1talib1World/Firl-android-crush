0bf8f65c707dfc738d4b7ef1a067217f19e803ff
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2023-10-24 18:08:07 +0100

    upd

--- app/src/main/java/com/Denzo/firl/MainActivity.java
+++ app/src/main/java/com/Denzo/firl/MainActivity.java
@@ -11,11 +11,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
-import com.Denzo.firl.Matches.MatchesFragment;
+import com.Denzo.firl.Matches.MatchesActivity;
 import com.Denzo.firl.Model.MatchPerson;
 import com.Denzo.firl.Model.MyMatchesPersons;
-import com.Denzo.firl.Swipe.SwipeFragment;
-import com.Denzo.firl.chat.ChatFragment;
+import com.Denzo.firl.chat.ChatActivity;
 import com.Denzo.firl.databinding.ActivityMainBinding;
 import com.Denzo.firl.helpers.HorizontalMarginItemDecoration;
 import com.Denzo.firl.listeners.MatchPersonClickListener;
@@ -49,9 +48,9 @@ public class MainActivity extends AppCompatActivity implements MatchPersonClickL
         uris.add(new StoryModel("https://picsum.photos/200/300", "Ankit Kumar", "12:00 PM"));
         uris.add(new StoryModel("https://picsum.photos/200/300", "Ankit Kumar", "12:00 PM"));
     }
-    SwipeFragment swipeFragment = new SwipeFragment();
-    MatchesFragment matchesFragment = new MatchesFragment();
-    ChatFragment chatFragment = new ChatFragment();
+    SwipeActivity swipeFragment = new SwipeActivity();
+    MatchesActivity matchesFragment = new MatchesActivity();
+    ChatActivity chatFragment = new ChatActivity();
 
 
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
