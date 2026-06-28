dc73fb78af8bd40bc258afa432192eb44cb9932a
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-06-28 20:05:33 +0100

    feat: update MyUtilsApp logic

---
+++ app/src/main/java/com/Denzo/firl/MyUtilsApp.java
@@ -0,0 +1,16 @@
+package com.Denzo.firl;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+public final class MyUtilsApp {
+    private MyUtilsApp(){}
+    public static void showToast(Context context, String message){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void showLog(String TAG,String message){
+        Log.d(TAG, "showLog: "+ message);
+    }
+}
