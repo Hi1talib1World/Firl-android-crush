67f57e2f962739ec04d69ee7f0492faa6f984a3c
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-06-28 20:05:36 +0100

    chore: update

---
+++ app/src/main/java/com/Denzo/firl/listeners/MatchPersonClickListener.java
@@ -0,0 +1,9 @@
+package com.Denzo.firl.listeners;
+
+import android.widget.ImageView;
+
+import com.Denzo.firl.Model.MatchPerson;
+
+public interface MatchPersonClickListener {
+    void onScrollPagerItemClick(MatchPerson personCard, ImageView imageView);
+}
